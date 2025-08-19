b8ce535afc3382556c67a566f0dd42f05987ac94
Eduardo Fornel <eduardofornel@gmail.com>
2025-08-18 23:13:47 -0300

    Corrigindo qrcode

--- src/bot.ts
+++ src/bot.ts
@@ -64,7 +64,12 @@ async function backToMenu(msg: Message) {
 }
 
 // ---------- EVENTOS ----------
+let qrShown = false;
+
 client.on('qr', (qr: string) => {
+  if (qrShown) return;
+  qrShown = true;
+
   console.log('QR recebido, escaneie para autenticar:');
   qrcode.generate(qr, { small: true });
 });
